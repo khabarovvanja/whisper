7f37ec91343d512daa7f76e80555890bd5dc5aa1
Ivan Khabarov <khabarovvanja@gmail.com>
2024-01-21 22:31:02 +0300

    create new repository and add all files

---
+++ whisperSTT.py
@@ -0,0 +1,22 @@
+import whisper
+import speech_recognition as sr
+import numpy as np 
+
+model = whisper.load_model('base')
+
+def main():
+    """
+    Функция распознавания речи в реальном времени
+    """
+    r = sr.Recognizer()
+    print('\033[32mГоворите...\033[0m')
+    while True:
+        with sr.Microphone(sample_rate=16000) as source:
+            audio = r.listen(source).get_raw_data()
+            buffer_audio = np.frombuffer(audio, dtype=np.int16).astype(np.float32) / 32768
+            phrase = model.transcribe(buffer_audio, fp16=False)
+            print(phrase['text'])
+
+
+if __name__=='__main__':
+    main()
